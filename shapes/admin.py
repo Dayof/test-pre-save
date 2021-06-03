f4f1e26a1afe634c7a7d0dc3b3aea7698509d5d8
Dayanne Fernandes <dayannefernandesc@gmail.com>
2021-06-02 21:24:17 -0300

    new model and post save example

--- shapes/admin.py
+++ shapes/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import ShapeA, ShapeB
+from .models import ShapeA, ShapeB, ShapeC
 
 
 admin.site.register(ShapeA)
 admin.site.register(ShapeB)
+admin.site.register(ShapeC)
--- shapes/models.py
+++ shapes/models.py
@@ -1,15 +1,6 @@
-from django.template.defaultfilters import slugify
-from django.db.models.signals import pre_save
+from django.db.models.signals import pre_save, post_save
 from django.dispatch import receiver
 from django.db import models
-
-
-class ShapeA(models.Model):
-    name = models.CharField(max_length=200)
-    size = models.IntegerField()
-
-    def __str__(self):
-        return f'name: {self.name}, size: {self.size}'
 
 
 class ShapeB(models.Model):
@@ -20,8 +11,27 @@ class ShapeB(models.Model):
        return f'name: {self.name}, size: {self.size}'
 
 
+class ShapeA(models.Model):
+    shapes_b = models.ManyToManyField(ShapeB, blank=True)
+    name = models.CharField(max_length=200)
+    size = models.IntegerField()
+
+    def __str__(self):
+        return f'name: {self.name}, size: {self.size}'
+
+
+class ShapeC(models.Model):
+    shapes_b_id = models.ForeignKey(ShapeB, on_delete=models.CASCADE)
+
+
 @receiver(pre_save, sender=ShapeA)
-def my_callback(sender, instance, *args, **kwargs):
+def callback_create_shapeb(sender, instance, *args, **kwargs):
     if instance.size == 5:
         new_obj = ShapeB(name=instance.name, size=instance.size)
         new_obj.save()
+
+
+@receiver(post_save, sender=ShapeB)
+def callback_create_shapec(sender, instance, *args, **kwargs):
+    new_obj = ShapeC.objects.create(shapes_b_id=instance)
+    new_obj.save()
